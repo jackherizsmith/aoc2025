27365f3fd1c2574db2bca86938e86ff76cc7a01f
Jack <jackherizsmith@gmail.com>
2025-12-02 13:34:05 +0000

    d2p2

--- src/day02/index.ts
+++ src/day02/index.ts
@@ -27,7 +27,40 @@ const part1 = (rawInput: string) => {
 
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
-  return;
+  const ranges = input.split(",");
+  const incorrectIds = [];
+  for (const range of ranges) {
+    const [min, max] = range.split("-").map(Number);
+    let id = min;
+    while (id <= max) {
+      const idString = id.toString();
+      const maxRepSize = idString.length / 2;
+      let rep = 1;
+      while (rep <= maxRepSize) {
+        if (idString.length % rep === 0) {
+          const segments = idString
+            .split("")
+            .reduce((segs: number[], char, i, arr) => {
+              if (i % rep === 0) {
+                let idSeg = "";
+                for (let j = i; j < i + rep; j++) {
+                  idSeg += arr[j];
+                }
+                segs.push(Number(idSeg));
+              }
+              return segs;
+            }, []);
+          if (segments.every((s) => s === segments[0])) {
+            incorrectIds.push(id);
+            break;
+          }
+        }
+        rep++;
+      }
+      id++;
+    }
+  }
+  return incorrectIds.reduce((a, b) => a + b, 0);
 };
 
 run({
@@ -42,10 +75,10 @@ run({
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124`,
+        expected: 4174379265,
+      },
     ],
     solution: part2,
   },
